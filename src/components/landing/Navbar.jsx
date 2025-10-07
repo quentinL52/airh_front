95d9ded35ef1878117f117ff830138a85c3089cd
quentinL52 <158512396+quentinL52@users.noreply.github.com>
2025-10-07 13:25:59 +0200

    navbar image

--- src/components/landing/Navbar.jsx
+++ src/components/landing/Navbar.jsx
@@ -23,7 +23,7 @@ function Navbar() {
             <nav className="navbar">
                 <div className="container">
                     <div className="navbar-brand">
-                        <img src="/images/AIRH_logo.png" alt="AIrh Logo" className="logo-icon" />
+                        <img src="/images/AIrh_logo.png" alt="AIrh Logo" className="logo-icon" />
                     </div>
                     <button onClick={handleAuthButtonClick} className="cta-button">
                         Se connecter / s'inscrire
